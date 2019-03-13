d0375690f94f379dd96fbe2ba0a46de8da0bd08e
Joy Lin <vvlnote@hotmail.com>
2019-03-13 00:38:38 +0000

    Done.

--- lib/values.rb
+++ lib/values.rb
@@ -11,8 +11,5 @@ require 'pry'
 def get_the_values(groceries)
   #code your solution here!
   values = []
-  groceries.each do |key, items|
-    items.each {|item| values << item}
-  end
-  values
+  values = groceries.values.flatten
 end
